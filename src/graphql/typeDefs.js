244deac999710140777fe0864d68849ac264a00b
Corey Ferguson <corey.t.ferguson@gmail.com>
2019-09-17 09:04:18 -0700

    updated graphql docs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,18 +2,34 @@
 module.exports = `
 
   type Query {
+
+    "The user currently authenticated."
     me: User
+
   }
 
   type Mutation {
-    upsertCard(userId: String!, id: String, deck: String, text: String): Card
+
+    "Create or update an existing card. Leave the id undefined to create a new card."
+    upsertCard(
+      userId: String!,
+      "When id is undefined a new card will be created."
+      id: String,
+      deck: String,
+      text: String
+    ): Card
+
   }
 
+  "You know, a user... like... you."
   type User {
+    "Primary key for a user. This is the sub defined by OpenID provider."
     sub: ID!
+    "Collection of cards belonging to this user"
     cards(pageSize: Int, next: String): CardCollection!
   }
 
+  "A Card. Represents a unit of knowledge a user maintains."
   type Card {
     id: ID!
     user: User
